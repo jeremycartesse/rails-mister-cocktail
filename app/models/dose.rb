1cbd82c4833941e1e04eb94caee608291ee2c96d
jeremycartesse <cartessejeremy@gmail.com>
2017-02-17 15:38:34 +0100

    Prepare for Heroku

---
+++ app/models/dose.rb
@@ -0,0 +1,10 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :description, presence: true
+  validates :cocktail_id, uniqueness: {scope: :ingredient_id}
+
+  def unity(unity="")
+    @unity = unity
+  end
+end
---
+++ db/migrate/20170216144438_add_reference_to_doses.rb
@@ -0,0 +1,5 @@
+class AddReferenceToDoses < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :doses, :ingredient, foreign_key: true
+  end
+end
